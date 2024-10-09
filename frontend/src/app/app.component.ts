0065c925e38ba786075a2b71d5a55d5701539cbb
LuaAguirre <renatoag98@gmail.com>
2024-10-08 23:10:57 -0500

    add input-url

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,29 +1,18 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
 import { ShortenerService } from './services/shortener.service';
-import type { Url } from './services/shortener.service';
 import { ShortededUrlComponent } from './components/shorteded-url/shorteded-url.component';
+import { InputUrlComponent } from './components/input-url/input-url.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MatSlideToggleModule,
-    MatButtonModule,
     MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    FormsModule,
     ShortededUrlComponent,
+    InputUrlComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -34,10 +23,8 @@ export class AppComponent {
   url = signal<string>('');
   shortededUrl = signal<string>('');
 
-  handleUrl() {
-    this.shortenerService.postUrl(this.url()).subscribe((data: Url) => {
-      this.shortededUrl.set(data.shortededUrl);
-      console.log(this.shortededUrl());
-    });
+  updateUrl(event: string) {
+    this.shortededUrl.set(event);
+    console.log('Valor recibido del componente hijo: ', this.shortededUrl());
   }
 }
--- frontend/src/app/components/input-url/input-url.component.ts
+++ frontend/src/app/components/input-url/input-url.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, model, output } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
+import { ShortenerService } from '../../services/shortener.service';
+import { Url } from '../../services/shortener.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-input-url',
   standalone: true,
-  imports: [MatFormFieldModule, MatIconModule, FormsModule],
+  imports: [
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './input-url.component.html',
   styleUrl: './input-url.component.scss',
 })
-export class InputUrlComponent {}
+export class InputUrlComponent {
+  shortenerService = inject(ShortenerService);
+  url = model<string>('');
+  shortededUrl = model<string>('');
+  onHandleUrl = output<string>();
+
+  handleUrl() {
+    this.shortenerService.postUrl(this.url()).subscribe((data: Url) => {
+      this.shortededUrl.set(data.shortededUrl);
+      console.log(this.shortededUrl());
+
+      this.onHandleUrl.emit(this.shortededUrl());
+    });
+  }
+}
