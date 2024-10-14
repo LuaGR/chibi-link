783d00237c9bfbde5c3cb4bf0bb9ab0f56c29d32
LuaAguirre <renatoag98@gmail.com>
2024-10-14 13:18:34 -0500

    add redirect

--- backend/shortener-url.js
+++ backend/shortener-url.js
@@ -1,12 +1,13 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
 const express = require('express')
 const cors = require('cors')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 const app = express()
 const port = 3000
 app.use(cors()); // Enable CORS
 app.use(express.json());
+
 app.post('/', async (req, res) => {
     const { url } = req.body
     const shortUrl = Math.random().toString(36).substr(2, 5)
@@ -23,6 +24,25 @@ app.post('/', async (req, res) => {
     }
 
 })
+
+app.get('/:shortId', async (req, res) => {
+    const { shortId } = req.params;
+
+    try {
+        const data = await prisma.link.findUnique({
+            where: { shortUrl: shortId },
+        });
+
+        if (!data) {
+            return res.status(404).json({ error: 'URL not found' }); // Devuelve un JSON con 404
+        }
+
+        return res.redirect(302, data.url); // Redirecciona a la url original directamente
+    } catch (error) {
+        console.error('Error fetching URL:', error);
+        return res.status(500).json({ error: 'Internal server error' }); // Devuelve un JSON con 500
+    }
+});
 
 //Habilitar puerto
 app.listen(port, () => {
