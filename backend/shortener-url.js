34a686019ff97ad22e877f34892ca3060c5c53ab
LuaAguirre <renatoag98@gmail.com>
2024-10-13 23:08:18 -0500

    update server/shortener-url

--- backend/shortener-url.js
+++ backend/shortener-url.js
@@ -1,13 +1,27 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 const express = require('express')
 const cors = require('cors')
+
 const app = express()
 const port = 3000
 app.use(cors()); // Enable CORS
 app.use(express.json());
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     const { url } = req.body
     const shortUrl = Math.random().toString(36).substr(2, 5)
-    res.status(200).send({ url, shortUrl })
+
+    try {
+        const data = await prisma.link.create({
+            data: { url, shortUrl }
+        })
+        return res.status(200).send(data)
+    }
+
+    catch (error) {
+        return res.status(500).send({ error })
+    }
+
 })
 
 //Habilitar puerto
