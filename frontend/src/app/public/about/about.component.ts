79192ef220ae487836616d7695a71142bc6e6df2
LuaAguirre <renatoag98@gmail.com>
2024-10-28 00:23:25 -0500

    add about

--- frontend/src/app/public/about/about.component.ts
+++ frontend/src/app/public/about/about.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'about',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatListModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
