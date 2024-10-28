0c57120147d6d22f72245d5a0b082dc512db6104
LuaAguirre <renatoag98@gmail.com>
2024-10-28 09:16:53 -0500

    update about imports

--- frontend/src/app/public/about/about.component.ts
+++ frontend/src/app/public/about/about.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'about',
   standalone: true,
-  imports: [MatCardModule, MatListModule],
+  imports: [MatCardModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
