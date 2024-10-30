5fe48f6a98b501a22c24d1887e23e2c625c27b0a
LuaAguirre <renatoag98@gmail.com>
2024-10-30 17:04:48 -0500

    add timeouts

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,15 @@ app.use(cors({
 
 app.use(express.json());
 
+// Middleware para manejar timeouts
+app.use((req, res, next) => {
+    res.setTimeout(5000, () => { // 5 segundos
+        console.log('Request has timed out.');
+        res.status(503).send('Server is busy, please try again later.');
+    });
+    next();
+});
+
 app.post('/', async (req, res) => {
     const { url } = req.body;
     const shortUrl = Math.random().toString(36).substr(2, 5);
