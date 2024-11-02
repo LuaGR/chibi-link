7807102e89d5f137520da356e97f20e77e63773a
LuaAguirre <renatoag98@gmail.com>
2024-11-02 02:57:57 -0500

    add ssr

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withViewTransitions()),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(), provideClientHydration(),
   ],
 };
