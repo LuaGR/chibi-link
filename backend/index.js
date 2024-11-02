b00e05fba98fa597f86bb12aea5295a94e9d4de7
LuaAguirre <renatoag98@gmail.com>
2024-11-02 16:09:09 -0500

    update cors

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,12 @@
 import express from 'express';
-import cors from 'cors'
+import { corsMiddleware } from './src/middlewares/cors.js';
 import routes from './src/routes/url.js'
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
-app.use(cors({
-    origin: (origin, callback) => {
-        const ACCEPTED_ORIGINS = ['https://chibi-link.vercel.app', 'http://localhost:4200']
-
-        if (ACCEPTED_ORIGINS.includes(origin)) {
-            return callback(null, true)
-        }
-
-        if (!origin) {
-            return callback(null, true)
-        }
-
-        return callback(new Error('Not allowed by CORS'))
-    }
-}));
+app.use(corsMiddleware);
 app.disable('x-powered-by')
 
 app.use((req, res, next) => {
